8858c90140e6f81b173046afccd7ef9feff261f6
Hamza Naeem and Joel Adams <joel@>
2018-04-04 10:05:03 -0700

    add project list to homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { ProjectService } from '../services/crowdFundProject.service'
 
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  providers: [ProjectService]
 })
 export class HomepageComponent implements OnInit {
   projects: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit() {
+    this.projects = this.projectService.getProjects();
   }
 
 }

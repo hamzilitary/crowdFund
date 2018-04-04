355f886a3a136c17da0e1da2ba56798cb6d915d5
Hamza Naeem and Joel Adams <hamza@>
2018-04-04 15:56:49 -0700

    add authentication to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
+import { AngularFireAuth } from 'angularfire2/auth'
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers: [AuthenticationService, AngularFireAuth]
 })
 export class NavbarComponent implements OnInit {
+  user;
+  private isLoggedIn: Boolean;
+  private userName: String;
 
-  constructor() { }
+   constructor(public authService: AuthenticationService) {
+     this.authService.user.subscribe(user => {
+       if (user == null) {
+         this.isLoggedIn = false;
+       } else {
+         this.isLoggedIn = true;
+         this.userName = user.displayName;
+       }
+     });
+   }
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 
   ngOnInit() {
   }

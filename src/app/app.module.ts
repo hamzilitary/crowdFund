6b9682e9bb0bc23d77d2cc6d61b290d164178822
Hamza Naeem and Joel Adams <hamza@>
2018-04-04 08:24:55 -0700

    initial setup, add navbar, add homepage component, implement routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/crowdFundProject.model.ts
@@ -0,0 +1,6 @@
+export class CrowdFundProj {
+
+  fundsRaised: 0;
+
+  constructor(name: string, starters: string[], desc: string, toRaise: number, willDo: string, swag: boolean){}
+}
